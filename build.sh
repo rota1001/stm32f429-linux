e068462eea187ff915794851aeeeca224b7fd38a
Chisheng Chen <johnny1001s000602@gmail.com>
2026-04-08 17:25:39 +0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+BUILDROOT_VERSION=2026.02
+
+ROOT_DIR="$(pwd)"
+BUILDROOT_DIR="$ROOT_DIR/buildroot-$BUILDROOT_VERSION"
+
+patch_buildroot() {
+    cp buildroot.config "$BUILDROOT_DIR/configs/stm32f429_disco_xip_defconfig"
+    cp linux.config "$BUILDROOT_DIR/board/stmicroelectronics/stm32f429-disco"
+    cp linux.patch "$BUILDROOT_DIR/board/stmicroelectronics/stm32f429-disco/patches/linux"
+    mkdir -p "$BUILDROOT_DIR/board/stmicroelectronics/stm32f429-disco/patches/uclibc"
+    cp flat-binary-xip.patch "$BUILDROOT_DIR/board/stmicroelectronics/stm32f429-disco/patches/uclibc"
+    cd "$BUILDROOT_DIR" && patch -p1 < "$ROOT_DIR/buildroot.patch"
+    cd "$ROOT_DIR"
+}
+
+fetch_sources() {
+    if [ ! -d "$BUILDROOT_DIR" ]; then
+        wget https://buildroot.org/downloads/buildroot-$BUILDROOT_VERSION.tar.xz
+        tar xvf buildroot-$BUILDROOT_VERSION.tar.xz
+        patch_buildroot
+    fi
+}
+
+build() {
+    make stm32f429_disco_xip_defconfig
+}
+
+fetch_sources
+patch_buildroot
+
